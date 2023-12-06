cc8d43c6ce0578936cd260488359194bc614ae2e
Damião Junior Terto <contato@damiaoterto.dev>
2023-12-06 17:15:22 -0300

    feat: implements lib functions

---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+#![deny(clippy::all)]
+
+#[macro_use]
+extern crate napi_derive;
+
+use qrcode_generator::QrCodeEcc;
+use napi::bindgen_prelude::*;
+use base64::{Engine as _, engine::general_purpose};
+
+#[napi]
+pub enum QRCorrectionLevel {
+	Low,
+	Medium,
+	Quartile,
+	High,
+}
+
+#[napi(object)]
+pub struct QrCodeOptions {
+  pub data: String,
+  pub ecc: QRCorrectionLevel,
+  pub size: Option<u32>
+}
+
+#[napi]
+pub struct QrGen {}
+
+#[napi]
+impl QrGen {
+  #[napi]
+  pub async fn to_url(options: QrCodeOptions) -> Result<String> {
+    let result = png_to_vec(options).unwrap();
+
+    let mime_type = "image/png";
+    let base64_encoded = general_purpose::STANDARD_NO_PAD.encode(result);
+
+    Ok(format!("data:{},base64,{}", mime_type, base64_encoded))
+  }
+
+  #[napi]
+  pub async fn to_buff(options: QrCodeOptions) -> Result<Buffer> {
+    let result = png_to_vec(options).unwrap();
+    Ok(result.into())
+  }
+
+  #[napi]
+  pub async fn to_file(options: QrCodeOptions, path: String) -> Result<()> {
+    png_to_file(options, path)
+  }
+}
+
+fn png_to_vec(options: QrCodeOptions) -> Result<Vec<u8>> {
+  let size = options.size.unwrap_or(1024) as usize;
+  
+  let result = qrcode_generator::to_png_to_vec(
+      options.data, 
+      From::from(options.ecc), 
+      size
+    ).unwrap();
+
+  Ok(result)
+}
+
+fn png_to_file(options: QrCodeOptions, path: String) -> Result<()> {
+  let size = options.size.unwrap_or(1024) as usize;
+
+  qrcode_generator::to_png_to_file(
+    options.data, 
+    From::from(options.ecc), 
+    size, 
+    path,
+  ).unwrap();
+
+  Ok(())
+}
+
+impl From<QRCorrectionLevel> for QrCodeEcc {
+    fn from(level: QRCorrectionLevel) -> Self {
+        match level {
+            QRCorrectionLevel::Low => QrCodeEcc::Low,
+            QRCorrectionLevel::Medium => QrCodeEcc::Medium,
+            QRCorrectionLevel::Quartile => QrCodeEcc::Quartile,
+            QRCorrectionLevel::High => QrCodeEcc::High,
+        }
+    }
+}
